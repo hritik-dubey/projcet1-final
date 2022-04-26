0e6e8ec23a1c9adff3d5efaeaa8b7de853f85720
bkrajor <bkrajor@gmail.com>
2022-04-26 20:54:33 +0530

    Let Start !

---
+++ src/routes/route.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const authorController= require("../controllers/authorController")
+const blogController = require('../controllers/blogController')
+
+
+router.post("/createAuthor", )
+
+router.post('/createBlog', )
+
+
+module.exports = router;
