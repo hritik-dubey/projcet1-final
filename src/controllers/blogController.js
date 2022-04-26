92bcf1fd2605f6f5efa049ba5d7e6348bb530d62
bkrajor <bkrajor@gmail.com>
2022-04-27 01:37:40 +0530

    DONE !

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,14 +1,46 @@
-const blogSchema = require("../models/blogModel")
+const blogModel = require("../models/blogModel")
 
 let createBlog = async (req, res) => {
     try {
-        let data=req.body
-        let result= await blogSchema.create(data)
-        res.status(201).send({Msg: result})
+        let data = req.body
+        let result = await blogModel.create(data)
+        res.status(201).send({ msg: result })
+    } catch (err) {
+        res.status(500).send({ Error: err.message })
+    }
+}
+
+let updateBlog = async (req, res) => {
+    try {
+        let bId = req.params.blogId
+        let blog = await blogModel.findOne({ _id: bId }, { isDeleted: false })
+        if (!blog)
+            return res.status(404).send({ status: false, msg: "No blog exits with this Id or the blog is deleted" })
+        let data = req.body
+        let updatedBlog = await blogModel.findByIdAndUpdate({ _id: bId }, { $set: data }, { new: true })
+        updatedBlog.isPublished = true
+        updatedBlog.publishedAt = Date()
+        updatedBlog.save()
+        res.status(200).send({ status: true, data: updatedBlog })
+    } catch (err) {
+        res.status(500).send({ Error: err.message })
+    }
+}
+
+let deleteBlog = async (req, res) => {
+    try {
+        let bId = req.params.blogId
+        let blog = await blogModel.findOne({ _id: bId }, { isDeleted: false })
+        if (!blog)
+            return res.status(404).send({ status: false, msg: "No blog exits with this Id or the blog is deleted" })
+        let deletedBlog = await blogModel.findByIdAndUpdate({ _id: bId }, { $set: { isDeleted: true } }, { new: true })
+        deletedBlog.deletedAt = Date()
+        deletedBlog.save()
+        res.status(200).send({ status: true, data: deletedBlog })
     } catch (err) {
         res.status(500).send({ Error: err.message })
     }
 }
 
 
-module.exports = { createBlog, }
+module.exports = { createBlog, updateBlog, deleteBlog }
