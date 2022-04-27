181f5da1551662d1ded641414d569acb15c32ea6
hritik-dubey <dubeyhritik32@gmail.com>
2022-04-27 22:15:52 +0530

    2 change by hritik

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -4,62 +4,60 @@ let getblog = async (req, res) => {
     try {
         let tittle = req.query.title;
         let givencategory = req.query.category;
-        // let giventags = req.query.tags;
-        // let data = await blogSchema.find(({ deleted: "true", published: "true" }));
-         res.status(201).send({ masg: data })
-          if (req.query) {
-            let resultdata = await blogSchema.find({ $or: [{ title: tittle }, { category: givencategory }, { tags: giventags }] });
+        let giventags = req.query.tags;
+        let data = await blogSchema.find(({ deleted: "true", published: "true" }));
+        if (req.query) {
+            let resultdata = await blogModel.find({ $or: [{ title: tittle }, { category: givencategory }, { tags: giventags }] });
             return res.status(200).send({ masg: resultdata })
         }
-      
-            
-        
-        let data = req.body
+        else {
+         let data = await blogModel.find(({ deleted: "true", published: "true" }));
         let result = await blogModel.create(data)
         res.status(201).send({ msg: result })
+    
+
     } catch (err) {
         res.status(500).send({ Error: err.message })
     }
 } 
 
-let updateBlog = async (req, res) => {
-    try {
-        let bId = req.params.blogId
-        let blog = await blogModel.findOne({ _id: bId }, { isDeleted: false })
-        if (!blog)
-            return res.status(404).send({ status: false, msg: "No blog exits with this Id or the blog is deleted" })
-        let data = req.body
-        let updatedBlog = await blogModel.findByIdAndUpdate({ _id: bId }, { $set: data }, { new: true })
-        updatedBlog.isPublished = true
-        updatedBlog.publishedAt = Date()
-        updatedBlog.save()
-        res.status(200).send({ status: true, data: updatedBlog })
-    } catch (err) {
-        res.status(500).send({ Error: err.message })
-    }
-}
-let postblog = async (req, res) => {
-    try {
-        let data = req.body;
-        let savedata = await blogSchema.create(data)
-        res.status(200).send({ masg: savedata })
+// let updateBlog = async (req, res) => {
+//     try {
+//         let bId = req.params.blogId
+//         let blog = await blogModel.findOne({ _id: bId }, { isDeleted: false })
+//         if (!blog)
+//             return res.status(404).send({ status: false, msg: "No blog exits with this Id or the blog is deleted" })
+//         let data = req.body
+//         let updatedBlog = await blogModel.findByIdAndUpdate({ _id: bId }, { $set: data }, { new: true })
+//         updatedBlog.isPublished = true
+//         updatedBlog.publishedAt = Date()
+//         updatedBlog.save()
+//         res.status(200).send({ status: true, data: updatedBlog })
+//     } catch (err) {
+//         res.status(500).send({ Error: err.message })
+//     }
+// }
+// let postblog = async (req, res) => {
+//     try {
+//         let data = req.body;
+//         let savedata = await blogSchema.create(data)
+//         res.status(200).send({ masg: savedata })
 
-let deleteBlog = async (req, res) => {
-    try {
-        let bId = req.params.blogId
-        let blog = await blogModel.findOne({ _id: bId }, { isDeleted: false })
-        if (!blog)
-            return res.status(404).send({ status: false, msg: "No blog exits with this Id or the blog is deleted" })
-        let deletedBlog = await blogModel.findByIdAndUpdate({ _id: bId }, { $set: { isDeleted: true } }, { new: true })
-        deletedBlog.deletedAt = Date()
-        deletedBlog.save()
-        res.status(200).send({ status: true, data: deletedBlog })
-    } catch (err) {
-        res.status(500).send({ Error: err.message })
-    }
-}
+// let deleteBlog = async (req, res) => {
+//     try {
+//         let bId = req.params.blogId
+//         let blog = await blogModel.findOne({ _id: bId }, { isDeleted: false })
+//         if (!blog)
+//             return res.status(404).send({ status: false, msg: "No blog exits with this Id or the blog is deleted" })
+//         let deletedBlog = await blogModel.findByIdAndUpdate({ _id: bId }, { $set: { isDeleted: true } }, { new: true })
+//         deletedBlog.deletedAt = Date()
+//         deletedBlog.save()
+//         res.status(200).send({ status: true, data: deletedBlog })
+//     } catch (err) {
+//         res.status(500).send({ Error: err.message })
+//     }
+// }
 
 
 module.exports.getblog = getblog;
-module.exports.postblog = postblog;
-module.exports = { createBlog, updateBlog, deleteBlog }
+//module.exports.postblog = postblog;
--- src/routes/route.js
+++ src/routes/route.js
@@ -6,15 +6,15 @@ const blogcontrollers = require("../controllers/blogController")
 
 
 router.get("/getblog",blogcontrollers.getblog);
-router.post("/postblog",blogcontrollers.postblog);
+// router.post("/postblog",blogcontrollers.postblog);
 
 
 
 
 
-router.put('/blogs:/blogId', blogController.createBlog)
+// router.put('/blogs:/blogId', blogController.createBlog)
 
-router.delete('/blogs:/blogId', blogController.deleteBlog)
+// router.delete('/blogs:/blogId', blogController.deleteBlog)
 
 
 module.exports = router; 
